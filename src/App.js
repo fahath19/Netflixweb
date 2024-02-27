c700486d5ac29a684e4a9fb1202503ec9537e22c
fahath19 <github241@gmail.com>
2024-02-27 11:36:26 +0530

    file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css'
+function App() {
+  return (
+   <div>
+    <h2 className='text-6xl'>hello</h2>
+   </div>
+  );
+}
+
+export default App;
